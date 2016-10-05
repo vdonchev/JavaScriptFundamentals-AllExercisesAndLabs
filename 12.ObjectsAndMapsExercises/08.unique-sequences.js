34dafbf72016113278f7da1db1c65ad377e93cd0
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-10-05 23:00:54 +0300

    optimize code

--- 12.ObjectsAndMapsExercises/08.unique-sequences.js
+++ 12.ObjectsAndMapsExercises/08.unique-sequences.js
@@ -4,15 +4,12 @@ function uniqueSequences(data) {
     for (let line of data) {
         let array = JSON.parse(line).map(Number).sort((a, b) => b - a);
         let toStore = `[${array.join(', ')}]`;
-        if (!arrays.has(toStore)) {
+        if (!arrays.has(toStore))
             arrays.set(toStore, array.length);
-        }
     }
 
-    let sortedKeys = [...arrays.keys()].sort((a, b) => customSort(a, b, arrays));
-    for (let arr of sortedKeys)
-        console.log(arr);
+    console.log([...arrays.keys()].sort((a, b) => customSort(a, b, arrays)).join('\n'));
 }
- 
-uniqueSequences(["[-3, -2, -1, 0, 1, 2, 3, 4]","[10, 1, -17, 0, 2, 13]","[4, -3, 3, -2, 2, -1, 1, 0]"]);
-uniqueSequences(["[7.14, 7.180, 7.339, 80.099]","[7.339, 80.0990, 7.140000, 7.18]","[7.339, 7.180, 7.14, 80.099]"]);
+
+uniqueSequences(["[-3, -2, -1, 0, 1, 2, 3, 4]", "[10, 1, -17, 0, 2, 13]", "[4, -3, 3, -2, 2, -1, 1, 0]"]);
+uniqueSequences(["[7.14, 7.180, 7.339, 80.099]", "[7.339, 80.0990, 7.140000, 7.18]", "[7.339, 7.180, 7.14, 80.099]"]);

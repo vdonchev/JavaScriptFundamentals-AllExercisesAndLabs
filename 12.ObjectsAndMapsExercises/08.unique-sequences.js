03a983d79d6d1d6335dedd504794c7503c4596e6
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-10-05 22:57:33 +0300

    remove redundant code

--- 12.ObjectsAndMapsExercises/08.unique-sequences.js
+++ 12.ObjectsAndMapsExercises/08.unique-sequences.js
@@ -13,6 +13,6 @@ function uniqueSequences(data) {
     for (let arr of sortedKeys)
         console.log(arr);
 }
-
+ 
 uniqueSequences(["[-3, -2, -1, 0, 1, 2, 3, 4]","[10, 1, -17, 0, 2, 13]","[4, -3, 3, -2, 2, -1, 1, 0]"]);
 uniqueSequences(["[7.14, 7.180, 7.339, 80.099]","[7.339, 80.0990, 7.140000, 7.18]","[7.339, 7.180, 7.14, 80.099]"]);
